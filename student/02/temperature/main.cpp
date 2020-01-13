33427d49f78d0f21571f8e388b0bac7d2d960840
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-01-13 14:27:01 +0200

    Celsius to Fahrenheit

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
+    int input;
     cout << "Enter a temperature: ";
+    cin >> input;
+    cout << input << " degrees Celsius is " << input * 1.8 + 32 << " degrees Fahrenheit" << endl;
 
     // Write your code here
 
