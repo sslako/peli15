79a9f96e718c2ab01291bd3ab11d2790564298c0
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-01-13 14:29:16 +0200

    Fahrenheit to celsius

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -8,7 +8,7 @@ int main()
     cout << "Enter a temperature: ";
     cin >> input;
     cout << input << " degrees Celsius is " << input * 1.8 + 32 << " degrees Fahrenheit" << endl;
-
+    cout << input << " degrees Fahrenheit is " << (input - 32) / 1.8 << " degrees Celsius" << endl;
     // Write your code here
 
     return 0;
