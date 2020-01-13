72f9beb0067760a5f0d0bfc55d15f0858d113835
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-01-13 15:11:07 +0200

    boing functionality added to number series game

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -7,8 +7,14 @@ int main()
     std::cout << "How many numbers would you like to have? ";
     std::cin >> input;
     while(toprint <= input){
-        if (toprint % 3 == 0){
-            std::cout << "zip" << std::endl;
+        if (toprint % 3 == 0 or toprint % 7 == 0){
+            if (toprint % 3 == 0){
+                std::cout << "zip ";
+            }
+            if (toprint % 7 == 0){
+                std::cout << "boing";
+            }
+            std::cout << std::endl;
         }
         else{
             std::cout << toprint << std::endl;
