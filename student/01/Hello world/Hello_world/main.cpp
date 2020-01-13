1b3e5341f4eeb9ae2f4d0b08b8e4aef41b34df6b
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-01-13 14:15:15 +0200

    Test

---
+++ student/01/Hello world/Hello_world/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Nollalla jako: " << 1/0 << endl;
+    return 0;
+}
---
+++ student/staattinen_tyypitys/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    int i;
+
+    if ( i = 0 ) {
+        cout << "Hurraa" << endl;
+    }
+    return 0;
+}
