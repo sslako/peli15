660c6fc406fd765d21dd5d24ee6f7335fa0478b3
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-01-17 11:47:24 +0200

    Fixed 0 in cube

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -7,11 +7,17 @@ int main()
     int input, cube;
     cout << "Enter a number: ";
     cin >> input;
-    cube = input * input * input;
-    if (cube / input / input != input){
-        cout << "Error! The cube of " << input << " is not " << cube << endl;
+    if (input == 0){
+        cout << "The cube of 0 is 0." << endl;
     }
     else{
-        cout << "The cube of " << input << " is " << cube << endl;
+        cube = input * input * input;
+        cout << cube;
+        if (cube / input / input != input){
+            cout << "Error! The cube of " << input << " is not " << cube << "." << endl;
+        }
+        else{
+            cout << "The cube of " << input << " is " << cube << "." << endl;
+        }
     }
 }
