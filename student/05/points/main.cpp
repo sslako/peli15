bc53a7cc4c89ba4e11d1e58ffe1598950b5c5d6b
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-02-16 14:06:19 +0200

    Fixed points

--- student/05/points/main.cpp
+++ student/05/points/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
@@ -14,26 +15,26 @@ int main()
     input_file.open(input_file_name);
 
     if (not input_file){
-        cout << "Error! The " << input_file_name << "cannot be opened." << endl;
+        cout << "Error! The file " << input_file_name << " cannot be opened." << endl;
         return EXIT_FAILURE;
     }
     string row, name;
-    string::size_type split_position;
+    int split_position;
     int points;
     map <string, int> tally;
     while (getline(input_file, row)){
         split_position = row.find(":");
-        name = row.substr(row.begin(), split_position - 1);
-        points = stoi(row.substr(split_position + 1, row.end()));
-        if (not tally.find(name)){
-            tally = {name, points};
+        name = row.substr(row[0], split_position);
+        points = stoi(row.substr(split_position));
+        if (tally.find(name) != tally.end()){
+            tally.insert({name, points});
         }
         else{
             tally.at(name) += points;
         }
     }
     for (auto pair : tally){
-        cout << pair.first() << ": " << pair.second << endl;
+        cout << pair.first << ": " << pair.second << endl;
     }
 
     input_file.close();
