5da218fdaaf0adcb7cfed49050a3096fc47eea86
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-02-23 18:53:20 +0200

    vertical

--- student/06/vertical/main.cpp
+++ student/06/vertical/main.cpp
@@ -7,12 +7,35 @@ using namespace std;
 
 void print_vertical(unsigned int num)
 {
-    RECURSIVE_FUNC
+    RECURSIVE_FUNC;
     // Do not remove RECURSIVE_FUNC declaration, it's necessary for automatic testing to work
     // ------------
 
 
     // Add your implementation here
+    int divisor = 1;
+    unsigned int remainder = 0;
+    while (remainder != num){
+        divisor *= 10 ;
+        remainder = num % divisor;
+    }
+    divisor /= 10;
+    unsigned int current_digit = num /divisor;
+    cout << current_digit << endl;
+    if (num != current_digit){
+        unsigned int new_num = num - divisor * current_digit;
+        int check_divisor = 1;
+        remainder = 0;
+        while (remainder != new_num){
+            check_divisor *= 10 ;
+            remainder = new_num % check_divisor;
+        }
+        while (check_divisor != divisor){
+            cout << 0 << endl;
+            check_divisor *= 10;
+        }
+        print_vertical(new_num);
+    }
 }
 
 // Do not modify rest of the code, or the automated testing won't work.
