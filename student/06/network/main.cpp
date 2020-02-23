8c3ce458fc374d63582dabf9a28eeb6d34f1f060
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-02-23 20:21:28 +0200

    netwrok

--- student/06/network/main.cpp
+++ student/06/network/main.cpp
@@ -2,6 +2,10 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
 
 const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
                               "C = count id\nD = depth id\n";
@@ -27,12 +31,42 @@ std::vector<std::string> split(const std::string& s, const char delimiter, bool 
     return result;
 }
 
+void print_subnetwork(map <string, set <string>>& recruiters, string name, int level = 1){
+    cout << name << endl;
+    for (string person : recruiters[name]){
+        for (int i = 0; i < level; ++i){
+            cout << "..";
+        }
+        print_subnetwork(recruiters, person, level + 1);
+    }
+}
 
+int count_subnetwork(map <string, set <string>>& recruiters, string name){
+    int total_network = 1;
+    for (string person : recruiters[name]){
+        total_network += count_subnetwork(recruiters, person);
+    }
+    return total_network;
+}
+
+int find_deepest_chain(map <string, set <string>>& recruiters, string name){
+    int deepest_chain = 0;
+    int provisional = 1;
+    for (string person : recruiters[name]){
+        provisional += find_deepest_chain(recruiters, person);
+        if(provisional > deepest_chain){
+            deepest_chain = provisional;
+            provisional = 1;
+        }
+    }
+    return deepest_chain;
+}
 
 int main()
 {
     // TODO: Implement the datastructure here
 
+    map <string, set <string>> recruiters;
 
     while(true){
         std::string line;
@@ -51,6 +85,8 @@ int main()
             std::string id2 = parts.at(2);
 
             // TODO: Implement the command here!
+            recruiters[id1].insert(id2);
+
 
         } else if(command == "P" or command == "p"){
             if(parts.size() != 2){
@@ -60,6 +96,7 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            print_subnetwork(recruiters, id);
 
         } else if(command == "C" or command == "c"){
             if(parts.size() != 2){
@@ -69,6 +106,7 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            cout << count_subnetwork(recruiters, id) - 1 << endl;
 
         } else if(command == "D" or command == "d"){
             if(parts.size() != 2){
@@ -78,6 +116,8 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+
+            cout << find_deepest_chain(recruiters, id) + 1 << endl;
 
         } else if(command == "Q" or command == "q"){
            return EXIT_SUCCESS;
