4fe77f4bd133b945590bb946fa658c653c24c4fc
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-03-14 14:20:21 +0200

    array operations

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -1,1 +1,50 @@
 #include "array_operations.hh"
+
+
+int greatest_v1(int* itemptr, int size){
+    int current, biggest = *itemptr;
+    for(int i = 1; i < size + 1; ++i, ++itemptr){
+        current = *itemptr;
+        if (current > biggest){
+            biggest = current;
+        }
+    }
+    return biggest;
+}
+
+int greatest_v2(int *itemptr, int *endptr){
+    int current, biggest = *itemptr;
+    itemptr += 1;
+    while(itemptr != endptr){
+        current = *itemptr;
+        if (current > biggest){
+            biggest = current;
+        }
+        itemptr += 1;
+    }
+    return biggest;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr){
+    int current = *itemptr;
+    while(itemptr != endptr){
+        *targetptr = current;
+        targetptr += 1;
+        itemptr += 1;
+        current = *itemptr;
+    }
+}
+
+void reverse(int *leftptr, int *rightptr){
+    int left = *leftptr, right = *(rightptr - 1);
+    while(leftptr != rightptr){
+        *leftptr = right;
+        *(rightptr - 1) = left;
+
+        leftptr += 1;
+        rightptr -= 1;
+
+        right = *(rightptr - 1);
+        left = *leftptr;
+    }
+}
