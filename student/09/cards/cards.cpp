ee9a51faf762043a78f952856fdf7b22d4a25094
Lauri Koivunen <lauri.j.koivunen@tuni.fi>
2020-03-27 14:56:25 +0200

    Cards works now?

--- student/09/cards/cards.cpp
+++ student/09/cards/cards.cpp
@@ -78,7 +78,7 @@ bool Cards::top_to_bottom(){
 
 // Prints the content of the data structure with ordinal numbers to the
 // output stream given as a parameter starting from the last element.
-void printFromBottomToTop(std::ostream& s){}
+void print_from_bottom_to_top(std::ostream& s);
 
 // A dynamic data structure must have a destructor
 // that can be called to deallocate memory,
